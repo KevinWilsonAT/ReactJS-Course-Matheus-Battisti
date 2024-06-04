719ecaea37c7afe722d547ffab0e7870dc1a207d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-06-03 23:31:53 -0300

    chore: upload class 03-06-24

---
+++ Projects/my-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello World (In React)</h1>
+        <p>This is my fisrt app (in React)</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
