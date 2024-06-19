a50217b54024e93c3a0bfc02ee8071c61e812d33
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-06-18 21:23:34 -0300

    chore: upload class 10-06-24

--- Projects/my-project/src/App.js
+++ Projects/my-project/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import HWorld from './components/HelloWorld';
+import TextPhrase from './components/TextPhrase';
 
 function App() {
+  const name = 'Kevin'
+  const newname = name.toUpperCase()
+
+  function sum(a, b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Hello World (In React)</h1>
-        <p>This is my fisrt app (in React)</p>
+        <p>This is my first app (in React)</p>
+        <h2>Updating JSX</h2>
+        <p>Hi, {name} (Normal).</p>
+        <p>Hi, {newname} (Uppercase).</p>
+        <p>Sum: {sum(1, 2)}</p>
+        <img src={url} alt="My Image" />
+        <TextPhrase />
+        <HWorld />
+        <TextPhrase />
       </header>
     </div>
   );
